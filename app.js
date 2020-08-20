1fe48976026232dc4463a8a8f94a630e73e2d606
jeff5489 <jeff5489@gmail.com>
2020-08-19 21:17:09 -0400

    changed response.json to .text

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 console.log('app.js is working')
+
+function getCraigslistFreeStuff(){
+    fetch('https://arcane-cliffs-49545.herokuapp.com/')
+     .then(function(response) {
+       return response.text();
+     })
+     .then(function(text) {
+       console.log(text);
+     });
+   };
+
+getCraigslistFreeStuff()
